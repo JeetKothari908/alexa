3ccfac9664eeff752cedaefc70a740a7d15b1bf1
Jeet Kothari <jkothari@cps.edu>
2021-11-24 20:06:12 -0600

    adding youtube functionality to alexa

--- alexa.py
+++ alexa.py
@@ -1,7 +1,7 @@
 import speech_recognition as sr
 import pyttsx3
 import datetime
-import pyautogui
+import pyautogui as pag
 import mouse
 import wikipedia
 import webbrowser
@@ -31,31 +31,30 @@ if 'what time is it' in text:
 
 elif "Korean pop" in text:
     #opens menu
-    pyautogui.moveTo(192, 1064, duration = .15)
+    pag.moveTo(192, 1064, duration = .15)
     mouse.click('left')
     #opens groove
-    pyautogui.moveTo(658, 380, duration = .25)
+    pag.moveTo(658, 380, duration = .25)
     mouse.click('left')
     #hits kpop
-    pyautogui.moveTo(54, 504, duration = 2.5)
+    pag.moveTo(54, 504, duration = 2.5)
     mouse.click('left')
     #hits play all
-    pyautogui.moveTo(801, 308, duration = 1)
+    pag.moveTo(801, 308, duration = 1)
     mouse.click('left')
 elif "play Korean pop" in text:
         #opens menu
-        pyautogui.moveTo(192, 1064, duration = .01)
+        pag.moveTo(192, 1064, duration = .15)
         mouse.click('left')
         #opens groove
-        pyautogui.moveTo(658, 380, duration = .25)
+        pag.moveTo(658, 380, duration = .25)
         mouse.click('left')
         #hits kpop
-        pyautogui.moveTo(54, 504, duration = 2.5)
+        pag.moveTo(54, 504, duration = 2.5)
         mouse.click('left')
         #hits play all
-        pyautogui.moveTo(801, 308, duration = 1)
+        pag.moveTo(801, 308, duration = 1)
         mouse.click('left')
-
 elif 'who is' in text:
     text = text.replace('who is',"")
     speak(wikipedia.summary(text,2))
@@ -75,9 +74,16 @@ elif 'Youtube' in text:
     #going to Youtube
     webbrowser.open_new_tab("https://www.youtube.com/")
     #clicking on search bar
+    pag.moveTo(596,170, duration = 0.75)
+    mouse.click('left')
     #typing text into search bar
-    #clicking searching
+    pag.typewrite(text, interval=0.05)
+    #clicking searching]
+    pag.moveTo(1260,170,interval=0.1)
+    mouse.click('left')
     #clicking first option
+    pag.moveTo(650,350,interval = 0.25)
+    mouse.click('left')
 #elif 'joke' in text: DO NOT USE PYJOKES- IT IS HORRIBLE
 #elif text == "what is the weather":
 # potentially make an alarm system using datetime and the text to speech thing
