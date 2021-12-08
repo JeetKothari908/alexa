6f453c94ce1756c6aa53b4033f0e97afe7bb1bc6
Jeet Kothari <jkothari@cps.edu>
2021-12-07 18:03:45 -0600

    making changes to alexa to keep browsers there after program ends.

--- alexa.py
+++ alexa.py
@@ -5,6 +5,13 @@ import pyautogui as pag
 import mouse
 import wikipedia
 import webbrowser
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options
+import Optionsimport speech_recognition as sr
 
 r = sr.Recognizer()
 with sr.Microphone() as source:
@@ -24,6 +31,11 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+#setting up selenium
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(chrome_options = chrome_options, executable_path=r'C:\Users\jeetd\downloads\chromedriver')
+#starting the if statements
 if 'what time is it' in text:
     time = datetime.datetime.now().strftime('%I %M %p')
     good = "it is", time
@@ -69,21 +81,15 @@ elif 'Google' in text:
     webbrowser.open_new_tab("https://www.google.com/search?q=" + text + "&start=")
 
 elif 'watch' in text:
-    #replacing text with text without youtube
     text = text.replace('watch',"")
-    #going to Youtube
-    webbrowser.open_new_tab("https://www.youtube.com/")
-    #clicking on search bar
-    pag.moveTo(596,170, duration = 2.5)
-    mouse.click('left')
-    #typing text into search bar
-    pag.typewrite(text, interval = 0.05)
-    #clicking searching]
-    pag.moveTo(1260,170,duration = 0.25)
-    mouse.click('left')
-    #clicking first option
-    pag.moveTo(650,350,duration = 3)
-    mouse.click('left')
+    #opens youtube and waits for it to load
+    driver.get('https://www.youtube.com/')
+    #finds the search bar using its html dom thing idk just click on the element and inspect it
+    search_bar = driver.find_element_by_name("search_query")
+    #puts stuff in search bar
+    search_bar.send_keys(text)
+    #clicks return
+    search_bar.send_keys(Keys.RETURN)
 #elif 'joke' in text: DO NOT USE PYJOKES- IT IS HORRIBLE
 #elif text == "what is the weather":
 # potentially make an alarm system using datetime and the text to speech thing
