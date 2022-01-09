eeb1c823243f9105b615dc1b264ef138bd12bf40
Jeet Kothari <jkothari@cps.edu>
2022-01-09 17:45:21 -0600

    added better way of recording, working like alexabot should, doing good

--- alexa.py
+++ alexa.py
@@ -14,16 +14,7 @@ from selenium.webdriver.chrome.options import Options
 import speech_recognition as sr
 import time
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    # read the audio data from the default microphone
-    audio_data = r.record(source, duration=5)
-    # record the data from the microphone for 5 seconds
-    print("Recognizing...")
-    # convert speech to text
-    text = r.recognize_google(audio_data)
-    print(text)
-#text = input('testing version, what command do you want to use? ')
+
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
@@ -32,79 +23,85 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-#setting up selenium
+def command():
+    r = sr.Recognizer()
+    while True:
+        with sr.Microphone() as source:
+            print("Alexa: Listening...")
+            audio=r.listen(source)
+            try:
+                text = r.recognize_google(audio)
+                print(text)
+                return text
+                break
+            except:
+                print("Try Again")
+while True:
+    text = command().lower() ## takes user command
+    #text = input('testing version, what do you want to do?')
+    #if 'alexa' in text:
+        #text = text.replace('alexa', "")
+    if 'what time is it' in text:
+        time = datetime.datetime.now().strftime('%I %M %p')
+        good = "it is", time
+        speak(good)
+
+    elif 'who is' in text:
+        text = text.replace('who is',"")
+        speak(wikipedia.summary(text,2))
+
+    elif 'Google' in text:
+        #replacing the text without google in it
+        text = text.replace('Google',"")
+        #searching up the thing in a new tab
+        chrome_options = Options()
+        chrome_options.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(chrome_options = chrome_options, executable_path=r'C:\Users\jeetd\downloads\chromedriver')
+        wait = WebDriverWait(driver, 25)
+
+        driver.get("https://www.google.com/search?q=" + text + "&start=")
+    elif 'google' in text:
+        #replacing the text without google in it
+        text = text.replace('google',"")
+        #searching up the thing in a new tab
+        chrome_options = Options()
+        chrome_options.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(chrome_options = chrome_options, executable_path=r'C:\Users\jeetd\downloads\chromedriver')
+        wait = WebDriverWait(driver, 25)
+
+        driver.get("https://www.google.com/search?q=" + text + "&start=")
+    elif 'watch' in text:
+        text = text.replace("watch", '')
+        text = text.replace(" ", '+')
+        youtub2 = 'https://www.youtube.com/results?search_query='
+        youtub2 = youtub2 + text
+        chrome_options = Options()
+        chrome_options.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(chrome_options = chrome_options, executable_path=r'C:\Users\jeetd\downloads\chromedriver')
+        wait = WebDriverWait(driver, 25)
+
+        driver.get(youtub2)
+        wait.until(expected_conditions.element_to_be_clickable((By.ID,"video-title" ))).click()
+
+    elif 'listen to' in text:
+        chrome_options = Options()
+        chrome_options.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(chrome_options = chrome_options, executable_path=r'C:\Users\jeetd\downloads\chromedriver')
+        text = text.replace('listen to',"")
+        wait = WebDriverWait(driver, 25)
+        text = text.replace(" ", '%20')
+        youtub2 = 'https://open.spotify.com/search/'
+        youtub2 = youtub2.append(text)
+        wait.until(expected_conditions.visibility_of_element_located((By.ID,"cidsyncjs" ))).click()
+
+    elif "bye" in text:
+        speak("Have a nice day ! ")
+        exit()
+    #elif 'joke' in text: DO NOT USE PYJOKES- IT IS HORRIBLE
+    #elif text == "what is the weather":
+    # potentially make an alarm system using datetime and the text to speech thing
+    #testing git commits
+    else:
+        speak('I cannot hear you')
 
 #starting the if statements
-if 'what time is it' in text:
-    time = datetime.datetime.now().strftime('%I %M %p')
-    good = "it is", time
-    speak(good)
-
-elif "Korean pop" in text:
-    #opens menu
-    pag.moveTo(192, 1064, duration = .15)
-    mouse.click('left')
-    #opens groove
-    pag.moveTo(658, 380, duration = .25)
-    mouse.click('left')
-    #hits kpop
-    pag.moveTo(54, 504, duration = 2.5)
-    mouse.click('left')
-    #hits play all
-    pag.moveTo(801, 308, duration = 1)
-    mouse.click('left')
-elif "play Korean pop" in text:
-    #opens menu
-    pag.moveTo(192, 1064, duration = .15)
-    mouse.click('left')
-    #opens groove
-    pag.moveTo(658, 380, duration = .25)
-    mouse.click('left')
-    #hits kpop
-    pag.moveTo(54, 504, duration = 2.5)
-    mouse.click('left')
-    #hits play all
-    pag.moveTo(801, 308, duration = 1)
-    mouse.click('left')
-elif 'who is' in text:
-    text = text.replace('who is',"")
-    speak(wikipedia.summary(text,2))
-elif 'is' in text:
-    text = text.replace('is',"")
-    speak(wikipedia.summary(text,2))
-
-elif 'Google' in text:
-    #replacing the text without google in it
-    text = text.replace('Google',"")
-    #searching up the thing in a new tab
-    webbrowser.open_new_tab("https://www.google.com/search?q=" + text + "&start=")
-
-elif 'watch' in text:
-    text = text.replace("watch", '')
-    text = text.replace(" ", '+')
-    youtub2 = 'https://www.youtube.com/results?search_query='
-    youtub2 = youtub2 + text
-    chrome_options = Options()
-    chrome_options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(chrome_options = chrome_options, executable_path=r'C:\Users\jeetd\downloads\chromedriver')
-    wait = WebDriverWait(driver, 25)
-    driver.get(youtub2)
-    wait.until(expected_conditions.element_to_be_clickable((By.ID,"video-title" ))).click()
-
-elif 'listen to' in text:
-    chrome_options = Options()
-    chrome_options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(chrome_options = chrome_options, executable_path=r'C:\Users\jeetd\downloads\chromedriver')
-    text = text.replace('listen to',"")
-    wait = WebDriverWait(driver, 25)
-    text = text.replace(" ", '%20')
-    youtub2 = 'https://open.spotify.com/search/'
-    youtub2 = youtub2.append(text)
-    wait.until(expected_conditions.visibility_of_element_located((By.ID,"cidsyncjs" ))).click()
-
-#elif 'joke' in text: DO NOT USE PYJOKES- IT IS HORRIBLE
-#elif text == "what is the weather":
-# potentially make an alarm system using datetime and the text to speech thing
-#testing git commits
-else:
-    speak('I cannot hear you')
--- ytmusic.py
+++ ytmusic.py
@@ -17,9 +17,9 @@ import time
 
 
 text = input('what do you want to listen to? ')
-text = text.replace('listen to',"")
-text = text.replace(" ", '%20')
-youtub2 = 'https://open.spotify.com/search/'
+#text = text.replace('listen to',"")
+text = text.replace(" ", '+')
+youtub2 = 'https://music.youtube.com/search?q='
 youtub2 = youtub2 + text
 #pag.moveTo(913, 545, 0.01)
 chrome_options = Options()
@@ -27,9 +27,12 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(chrome_options = chrome_options, executable_path=r'C:\Users\jeetd\downloads\chromedriver')
 driver.get(youtub2)
 wait = WebDriverWait(driver, 25)
-a = ActionChains(driver)
-wait.until(expected_conditions.visibility_of_element_located((By.className,"spotify-logo--text" )))
-play = find_element_by_id('play-button')
-a.move_to_element(play).perform()
+#a = ActionChains(driver)
+#wait.until(expected_conditions.element_to_be_clickable((By.ID,"ripple" )))
 time.sleep(1)
-div.click()
+#figure out how to use xpath and then use it!
+thing = driver.find_element_by_class_name("yt-simple-endpoint style-scope ytmusic-responsive-list-item-renderer")
+thing.click()
+wait.until(expected_conditions.element_to_be_clickable((By.name,"tp-yt-paper-button" ))).click()
+#thing2 = driver.find_element_by_name('tp-yt-paper-button')
+#thing2.click()
